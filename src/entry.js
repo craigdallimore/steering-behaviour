3259b34f871abeb703f3d1cb2393fffdc8800947
craigdallimore <decoy9697@gmail.com>
2021-06-08 21:02:20 +1200

    Animate, add favicon

--- src/entry.js
+++ src/entry.js
@@ -1,21 +1,31 @@
 // @flow
 import drawScene from "../lib/drawScene.js";
 
-function main(canvas /* :HTMLCanvasElement */) {
+function getState(time) {
+  return {
+    character: {
+      x: 100,
+      z: 300 - ((time / 10) % 300),
+    },
+  };
+}
+
+function main(canvas: HTMLCanvasElement) {
   const ctx = canvas.getContext("2d");
   ctx.translate(0.5, 0.5);
 
-  drawScene(
-    ctx,
-    {
-      x: 100,
-      z: 100,
-    },
-    {
+  function frame(time) {
+    ctx.clearRect(0, 0, 300, 300);
+    const { character } = getState(time);
+
+    drawScene(ctx, character, {
       x: 140,
       z: 140,
-    }
-  );
+    });
+    window.requestAnimationFrame(frame);
+  }
+
+  window.requestAnimationFrame(frame);
 }
 
 const c = document.querySelector("#can");
