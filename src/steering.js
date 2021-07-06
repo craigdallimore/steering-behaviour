dd038385d160db75fe9c96a533bb5cdc08f9c74d
craigdallimore <decoy9697@gmail.com>
2021-07-06 20:45:09 +1200

    Add match-velocity implementation
    Start refactoring steering configs

--- src/steering.js
+++ src/steering.js
@@ -19,12 +19,13 @@ export type Steering = {
 
 // SEEK -----------------------------------------------------------------------
 
-const maxAcceleration = 25;
-
 export function getSeekSteering(
   character: Kinematic,
   target: Kinematic
 ): Steering {
+  // Config
+  const maxAcceleration = 25;
+
   const linear = multiply(
     normalise(subtract(target.position, character.position)),
     maxAcceleration
@@ -40,14 +41,17 @@ export function getSeekSteering(
 
 // ARRIVE ---------------------------------------------------------------------
 
-const maxSpeed = 55;
-const targetRadius = 5;
-const slowRadius = 60;
-
 export function getArriveSteering(
   character: Kinematic,
   target: Kinematic
 ): Steering | null {
+  // Config
+  const maxAcceleration = 25;
+  const timeToTarget = 30;
+  const maxSpeed = 55;
+  const targetRadius = 5;
+  const slowRadius = 60;
+
   const distanceToTarget = distance(character.position, target.position);
   const directionToTarget = subtract(target.position, character.position);
 
@@ -67,7 +71,7 @@ export function getArriveSteering(
   const reduced = subtract(idealVelocity, character.velocity);
 
   // A higher value will arrive sooner
-  const linear = multiply(reduced, 0.3);
+  const linear = multiply(reduced, 1 / timeToTarget);
 
   const finalLinear =
     length(linear) > maxAcceleration
@@ -95,16 +99,15 @@ function mapToRange(orientation: number): number {
 
 // ALIGN ----------------------------------------------------------------------
 
-const timeToTarget = 0.1;
-const maxAngularAcceleration = 140;
-const maxRotation = 120;
-const decelerationTolerance = 2;
-const alignTolerance = 0.01;
-
 export function getAlignSteering(
   character: Kinematic,
   target: Kinematic
 ): Steering {
+  const maxAngularAcceleration = 140;
+  const maxRotation = 120;
+  const decelerationTolerance = 2;
+  const alignTolerance = 0.01;
+  const timeToTarget = 0.1;
   const linear = [0, 0];
 
   const rotation = mapToRange(target.orientation - character.orientation);
@@ -145,11 +148,20 @@ export function getMatchVelocitySteering(
   character: Kinematic,
   target: Kinematic
 ): Steering {
-  const linear = [0, 0];
+  // Config
+  const timeToTarget = 0.1;
+  const maxAcceleration = 25;
+
   const angular = 0;
+  const linear = subtract(target.velocity, character.velocity);
+  const dividedLinear = multiply(linear, 1 / timeToTarget);
+  const finalLinear =
+    length(dividedLinear) > maxAcceleration
+      ? multiply(normalise(linear), maxAcceleration)
+      : linear;
 
   return {
     angular,
-    linear,
+    linear: finalLinear,
   };
 }
