4c617d0ae307268af2b7f3dad11284f313ef7599
craigdallimore <decoy9697@gmail.com>
2021-07-12 20:26:43 +1200

    Draw a basic path

--- examples/main/runEffects.js
+++ examples/main/runEffects.js
@@ -2,6 +2,7 @@
 
 import drawArrow from "../../lib/drawArrow.js";
 import drawGrid from "../../lib/drawGrid.js";
+import drawPath from "../../lib/drawPath.js";
 
 import type { State } from "./state.js";
 
@@ -19,4 +20,15 @@ export default function runEffects(
 
   // Character
   drawArrow(dom.main, state.character);
+
+  drawPath(
+    dom.main,
+    [
+      [500, 10],
+      [410, 40],
+      [140, 270],
+      [650, 600],
+    ],
+    "rgba(0,0,0,0.5)"
+  );
 }
