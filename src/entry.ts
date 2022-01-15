8131c459bbaebb96351965fdbb683160365a205b
craigdallimore <decoy9697@gmail.com>
2022-01-15 18:31:53 +1300

    Convert basic frame loop

--- src/entry.ts
+++ src/entry.ts
@@ -1,1 +1,13 @@
-console.log("hi from typescript");
+function main(): void {
+  function frame(prevtime: number) {
+    return function (time: number) {
+      const t = (time - prevtime) / 1000;
+      console.log({ t });
+      window.requestAnimationFrame(frame(time));
+    };
+  }
+
+  window.requestAnimationFrame(frame(0));
+}
+
+main();
