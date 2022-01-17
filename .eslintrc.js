e113c4b9e5193ca7a237cf2ee6028adb632b99af
craigdallimore <decoy9697@gmail.com>
2022-01-17 17:48:11 +1300

    Update lint rules and plugins for typescript

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: ["eslint:recommended", "prettier"],
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+  ],
+  plugins: ["@typescript-eslint"],
   parserOptions: {
     ecmaVersion: 12,
     sourceType: "module",
