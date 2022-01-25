02e58fd409bf7a30d2781c31e146a153b532b459
craigdallimore <decoy9697@gmail.com>
2022-01-25 18:28:50 +1300

    Translate canvas and use a ref for ctx

--- src/components/Canvas.tsx
+++ src/components/Canvas.tsx
@@ -4,15 +4,23 @@ import * as app from "../main/index.js";
 
 const Canvas = (props: { state: State }) => {
   const canvasRef = React.useRef<HTMLCanvasElement | null>(null);
+  const ctxRef = React.useRef<CanvasRenderingContext2D | null>(null);
+
+  React.useEffect(() => {
+    if (ctxRef.current) {
+      app.runEffects(ctxRef.current, props.state);
+    }
+  }, [props.state.lastUpdated]);
 
   React.useEffect(() => {
     if (canvasRef.current) {
       const ctx = canvasRef.current.getContext("2d");
       if (ctx) {
-        app.runEffects(ctx, props.state);
+        ctx.translate(0.5, 0.5);
+        ctxRef.current = ctx;
       }
     }
-  }, [props.state.lastUpdated]);
+  }, [canvasRef.current]);
 
   return (
     <canvas ref={canvasRef} width="800" height="800" id="canvas-main"></canvas>
