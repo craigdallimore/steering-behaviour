4ebb422bd805849027fa1d20e04de16b97dba580
craigdallimore <decoy9697@gmail.com>
2022-02-03 10:18:08 +1300

    Give the character sidebar an empty state

--- src/components/CharacterSidebar.tsx
+++ src/components/CharacterSidebar.tsx
@@ -26,99 +26,101 @@ const CharacterSidebar = () => {
 
   return (
     <aside className="character-sidebar" aria-label="Character sidebar">
-      <h2>Selected item</h2>
-      <form className={className}>
-        <fieldset>
-          <legend>
-            Kinematic <code>{idText}</code>
-          </legend>
+      <h2>{focussedCharacter ? "Selected Item" : "Nothing selected"}</h2>
+      {focussedCharacter && (
+        <form className={className}>
+          <fieldset>
+            <legend>
+              Kinematic <code>{idText}</code>
+            </legend>
 
-          <label htmlFor="orientation">Orientation</label>
-          <input
-            id="orientation"
-            type="range"
-            min="-2"
-            max="2"
-            step="0.1"
-            value={focussedCharacter?.kinematic.orientation.toString()}
-            onChange={(e) => {
-              if (e.target instanceof HTMLInputElement) {
+            <label htmlFor="orientation">Orientation</label>
+            <input
+              id="orientation"
+              type="range"
+              min="-2"
+              max="2"
+              step="0.1"
+              value={focussedCharacter?.kinematic.orientation.toString()}
+              onChange={(e) => {
+                if (e.target instanceof HTMLInputElement) {
+                  dispatch({
+                    type: "ORIENTATION_CHANGED",
+                    payload: parseFloat(e.target.value),
+                  });
+                }
+              }}
+            />
+
+            <NumericField
+              label="Rotation"
+              id="rotation"
+              value={focussedCharacter?.kinematic.rotation}
+              onChange={(payload) => {
                 dispatch({
-                  type: "ORIENTATION_CHANGED",
-                  payload: parseFloat(e.target.value),
+                  type: "ROTATION_CHANGED",
+                  payload,
                 });
-              }
-            }}
-          />
+              }}
+            />
 
-          <NumericField
-            label="Rotation"
-            id="rotation"
-            value={focussedCharacter?.kinematic.rotation}
-            onChange={(payload) => {
-              dispatch({
-                type: "ROTATION_CHANGED",
-                payload,
-              });
-            }}
-          />
+            <NumericField
+              label="Horizontal position"
+              id="position-x"
+              min={0}
+              max={800}
+              value={focussedCharacter?.kinematic.position[0]}
+              onChange={(payload) => {
+                dispatch({
+                  type: "POSX_CHANGED",
+                  payload,
+                });
+              }}
+            />
 
-          <NumericField
-            label="Horizontal position"
-            id="position-x"
-            min={0}
-            max={800}
-            value={focussedCharacter?.kinematic.position[0]}
-            onChange={(payload) => {
-              dispatch({
-                type: "POSX_CHANGED",
-                payload,
-              });
-            }}
-          />
+            <NumericField
+              label="Vertical position"
+              id="position-z"
+              min={0}
+              max={800}
+              value={focussedCharacter?.kinematic.position[1]}
+              onChange={(payload) => {
+                dispatch({
+                  type: "POSZ_CHANGED",
+                  payload,
+                });
+              }}
+            />
 
-          <NumericField
-            label="Vertical position"
-            id="position-z"
-            min={0}
-            max={800}
-            value={focussedCharacter?.kinematic.position[1]}
-            onChange={(payload) => {
-              dispatch({
-                type: "POSZ_CHANGED",
-                payload,
-              });
-            }}
-          />
+            <NumericField
+              label="Horizontal velocity"
+              id="vel-x"
+              value={focussedCharacter?.kinematic.velocity[0]}
+              onChange={(payload) => {
+                dispatch({
+                  type: "VELX_CHANGED",
+                  payload,
+                });
+              }}
+            />
 
-          <NumericField
-            label="Horizontal velocity"
-            id="vel-x"
-            value={focussedCharacter?.kinematic.velocity[0]}
-            onChange={(payload) => {
-              dispatch({
-                type: "VELX_CHANGED",
-                payload,
-              });
-            }}
-          />
-
-          <NumericField
-            label="Vertical velocity"
-            id="vel-z"
-            value={focussedCharacter?.kinematic.velocity[1]}
-            onChange={(payload) => {
-              dispatch({
-                type: "VELZ_CHANGED",
-                payload,
-              });
-            }}
-          />
-        </fieldset>
-        {focussedCharacter && (
-          <Behaviours dispatch={dispatch} character={focussedCharacter} />
-        )}
-      </form>
+            <NumericField
+              label="Vertical velocity"
+              id="vel-z"
+              value={focussedCharacter?.kinematic.velocity[1]}
+              onChange={(payload) => {
+                dispatch({
+                  type: "VELZ_CHANGED",
+                  payload,
+                });
+              }}
+            />
+          </fieldset>
+          {focussedCharacter && (
+            <Behaviours dispatch={dispatch} character={focussedCharacter} />
+          )}
+        </form>
+      )}
     </aside>
   );
 };
