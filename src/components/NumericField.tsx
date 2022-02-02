cf69edb3b06e74e6c7b3b07762db74e47d6c38e6
craigdallimore <decoy9697@gmail.com>
2022-02-02 21:45:40 +1300

    Address poor typing experience

--- src/components/NumericField.tsx
+++ src/components/NumericField.tsx
@@ -10,15 +10,36 @@ type Props = {
 };
 
 const NumericField = (props: Props) => {
+  const [isFocussed, setIsFocussed] = React.useState(false);
+
+  // A bit gnarly.
+  // The value we show is fixed to two decimal places to make it easier to read.
+  // The system will still represent it as the full 3.00000001 under the hood.
+  // When the user is typing (and the input has focus) we stop using .toFixed on
+  // the value as it interferes with the typing experience.
+  const value = props.value
+    ? isFocussed
+      ? props.value
+      : props.value.toFixed(2)
+    : undefined;
+
   return (
     <>
       <label htmlFor={props.id}>{props.label}</label>
       <input
+        onFocus={(e) => {
+          setIsFocussed(true);
+          if (e.target instanceof HTMLInputElement) {
+            props.onChange(parseFloat(e.target.value));
+          }
+        }}
+        step="any"
+        onBlur={() => setIsFocussed(false)}
         id={props.id}
         type="number"
         min={props.min}
         max={props.max}
-        value={props.value ? props.value.toFixed(2) : undefined}
+        value={value}
         onChange={(e) => {
           if (e.target instanceof HTMLInputElement) {
             props.onChange(parseFloat(e.target.value));
