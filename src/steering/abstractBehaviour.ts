009e801fd8cbab4e66c780c8d512369b9ea70167
craigdallimore <decoy9697@gmail.com>
2022-02-09 14:13:34 +1300

    Fix issue where the UI would not update the model

--- src/steering/abstractBehaviour.ts
+++ src/steering/abstractBehaviour.ts
@@ -1,5 +1,7 @@
 import type { SteeringBehaviourName } from "@domain/types";
+import { immerable } from "immer";
 
 export abstract class AbstractBehaviour {
   abstract readonly name: SteeringBehaviourName;
+  [immerable] = true;
 }
