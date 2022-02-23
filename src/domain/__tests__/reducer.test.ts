9b265a77944435a998a6d82aa9d992e64da519e2
craigdallimore <decoy9697@gmail.com>
2022-02-23 18:18:05 +1300

    Add light tests for more reducer paths

--- src/domain/__tests__/reducer.test.ts
+++ src/domain/__tests__/reducer.test.ts
@@ -1,12 +1,45 @@
 import { initialState } from "@domain/initialState";
 import { reducer } from "@domain/reducer";
+import { Vector } from "@domain/types";
+import getFocussedCharacter from "@lib/getFocussedCharacter";
 
 Object.freeze(initialState);
 
 /*
-test('TICK', () => {});
-test('PLAY_BUTTON_CLICKED', () => {});
+describe('TICK', () => {});
 */
+
+describe("PLAY_BUTTON_CLICKED", () => {
+  it("changes the play state from false to true", () => {
+    const changedState = {
+      ...initialState,
+      ui: {
+        ...initialState.ui,
+        isPaused: false,
+      },
+    };
+    const nextState = reducer(changedState, {
+      type: "PLAY_BUTTON_CLICKED",
+    });
+
+    expect(nextState.ui.isPaused).toEqual(true);
+  });
+  it("changes the play state from true to false", () => {
+    const changedState = {
+      ...initialState,
+      ui: {
+        ...initialState.ui,
+        isPaused: true,
+      },
+    };
+    const nextState = reducer(changedState, {
+      type: "PLAY_BUTTON_CLICKED",
+    });
+
+    expect(nextState.ui.isPaused).toEqual(false);
+  });
+});
+
 describe("RESET_BUTTON_CLICKED", () => {
   it("restores initial value of the scenario", () => {
     const changedState = {
@@ -44,9 +77,34 @@ describe("DEBUG_MODE_CHANGED", () => {
     expect(nextState.ui.isDebugMode).toBe(true);
   });
 });
+
+describe.skip("SCENARIO_CHANGED", () => {
+  it("replaces the current scenario and focussed scenario id", () => {
+    const nextState = reducer(initialState, {
+      type: "SCENARIO_CHANGED",
+      payload: "SC_WANDER",
+    });
+    expect(nextState.ui.focussedScenarioId).toBe("SC_WANDER");
+    expect(nextState.ui.focussedScenarioId).not.toBe(
+      initialState.ui.focussedScenarioId
+    );
+    expect(nextState.scenario?.name).not.toBe(initialState.scenario?.name);
+  });
+});
 /*
 describe('CANVAS_CLICKED', () => {});
-describe('CANVAS_RESIZED', () => {});
+*/
+describe("CANVAS_RESIZED", () => {
+  it("updates the stored dimensions of the canvas", () => {
+    const newDimensions: Vector = [120, 320];
+    const nextState = reducer(initialState, {
+      type: "CANVAS_RESIZED",
+      payload: newDimensions,
+    });
+    expect(nextState.ui.canvasDimensions).toBe(newDimensions);
+  });
+});
+/*
 describe("BEHAVIOUR_REMOVED", () => {
   it("given the last behaviour is removed, the character will stop moving", () => {
     const scenario = initBlankScenario();
@@ -79,11 +137,128 @@ describe("BEHAVIOUR_REMOVED", () => {
     expect(char?.kinematic.velocity).toEqual([0, 0]);
   });
 });
-describe('SCENARIO_CHANGED', () => {});
-describe('ORIENTATION_CHANGED', () => {});
-describe('ROTATION_CHANGED', () => {});
-describe('POSX_CHANGED', () => {});
-describe('POSZ_CHANGED', () => {});
-describe('VELX_CHANGED', () => {});
-describe('VELZ_CHANGED', () => {});
 */
+describe("ORIENTATION_CHANGED", () => {
+  it("updates the orientation of the focussed character", () => {
+    const nextOrientation = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.orientation).not.toEqual(nextOrientation);
+
+    const nextState = reducer(initialState, {
+      type: "ORIENTATION_CHANGED",
+      payload: nextOrientation,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.orientation).toBe(nextOrientation);
+  });
+});
+describe("ROTATION_CHANGED", () => {
+  it("updates the rotation of the focussed character", () => {
+    const nextRotation = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.rotation).not.toEqual(nextRotation);
+
+    const nextState = reducer(initialState, {
+      type: "ROTATION_CHANGED",
+      payload: nextRotation,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.rotation).toBe(nextRotation);
+  });
+});
+describe("POSX_CHANGED", () => {
+  it("updates the x position of the focussed character", () => {
+    const nextPosition = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.position[0]).not.toEqual(nextPosition);
+
+    const nextState = reducer(initialState, {
+      type: "POSX_CHANGED",
+      payload: nextPosition,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.position[0]).toBe(nextPosition);
+  });
+});
+describe("POSZ_CHANGED", () => {
+  it("updates the z position of the focussed character", () => {
+    const nextPosition = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.position[1]).not.toEqual(nextPosition);
+
+    const nextState = reducer(initialState, {
+      type: "POSZ_CHANGED",
+      payload: nextPosition,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.position[1]).toBe(nextPosition);
+  });
+});
+describe("VELX_CHANGED", () => {
+  it("updates the x velocity of the focussed character", () => {
+    const nextVelocity = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.velocity[0]).not.toEqual(nextVelocity);
+
+    const nextState = reducer(initialState, {
+      type: "VELX_CHANGED",
+      payload: nextVelocity,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.velocity[0]).toBe(nextVelocity);
+  });
+});
+describe("VELZ_CHANGED", () => {
+  it("updates the z velocity of the focussed character", () => {
+    const nextVelocity = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.velocity[1]).not.toEqual(nextVelocity);
+
+    const nextState = reducer(initialState, {
+      type: "VELZ_CHANGED",
+      payload: nextVelocity,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.velocity[1]).toBe(nextVelocity);
+  });
+});
+describe("MAX_ACCELERATION_CHANGED", () => {
+  it("updates the max acceleration of the focussed character", () => {
+    const nextMaxAcceleration = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.maxAcceleration).not.toEqual(nextMaxAcceleration);
+
+    const nextState = reducer(initialState, {
+      type: "MAX_ACCELERATION_CHANGED",
+      payload: nextMaxAcceleration,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.maxAcceleration).toBe(nextMaxAcceleration);
+  });
+});
+describe("MAX_ANGULAR_ACCELERATION_CHANGED", () => {
+  it("updates the max angular acceleration of the focussed character", () => {
+    const nextMaxAngularAcceleration = 3;
+    const char = getFocussedCharacter(initialState);
+    expect(char?.kinematic.maxAngularAcceleration).not.toEqual(
+      nextMaxAngularAcceleration
+    );
+
+    const nextState = reducer(initialState, {
+      type: "MAX_ANGULAR_ACCELERATION_CHANGED",
+      payload: nextMaxAngularAcceleration,
+    });
+
+    const nextChar = getFocussedCharacter(nextState);
+    expect(nextChar?.kinematic.maxAngularAcceleration).toBe(
+      nextMaxAngularAcceleration
+    );
+  });
+});
