2f095dace1474a1d979edc4ce8a995de7f84b771
craigdallimore <decoy9697@gmail.com>
2022-03-17 17:24:02 +1300

    Lint

--- src/components/__tests__/AssignedTarget.test.tsx
+++ src/components/__tests__/AssignedTarget.test.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { render, userEvent } from "@test-utils";
+import { render } from "@test-utils";
 import AssignedTarget from "../AssignedTarget";
 
 describe("AssignedTarget", () => {
