18bb254f49709f05be9fcc5f07883b7bea83398d
craigdallimore <decoy9697@gmail.com>
2022-04-12 11:48:05 +1200

    Update shell

--- shell.nix
+++ shell.nix
@@ -1,7 +1,6 @@
 let
 pkgs = import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixos-21.11.tar.gz") {};
-in pkgs.stdenv.mkDerivation {
-    name = "boilerplate";
+in pkgs.mkShell {
     buildInputs = [
       pkgs.yarn        #coc.vim
       pkgs.nodejs-14_x #coc.vim
@@ -10,6 +9,8 @@ in pkgs.stdenv.mkDerivation {
       alias gst='git status'
       alias gd='git diff'
       alias gc='git commit'
+      alias gb='git branch'
+      alias gco='git checkoout'
       :q() {
         exit
       }
