8e614123ddaf92ad45aebbf00ba3538424134a5c
craigdallimore <decoy9697@gmail.com>
2022-04-12 11:24:34 +1200

    Exclude livereload from production, silly

--- rollup.config.js
+++ rollup.config.js
@@ -1,3 +1,4 @@
+import fs from "fs";
 import typescript from "@rollup/plugin-typescript";
 import serve from "rollup-plugin-serve";
 import sourcemaps from "rollup-plugin-sourcemaps";
@@ -11,31 +12,51 @@ import external from "rollup-plugin-peer-deps-external";
 import replace from "@rollup/plugin-replace";
 import livereload from "rollup-plugin-livereload";
 
-export default {
-  input: "src/main.tsx",
-  output: {
-    file: "dist/bundle.js",
-    format: "cjs",
-    sourcemap: true,
+const envPlugins = process.env.NODE_ENV === "development" ? [livereload()] : [];
+
+function buildHTML() {
+  return {
+    name: "build-html",
+    buildStart: async function buildStart() {
+      const content = await fs.promises.readFile("./html/index.html", "utf-8");
+      const nextHtml =
+        process.env.NODE_ENV === "development"
+          ? content
+          : content.replace(
+              '<script src="http://localhost:35729/livereload.js"></script>',
+              ""
+            );
+      return await fs.promises.writeFile("./dist/index.html", nextHtml);
+    },
+  };
+}
+
+export default [
+  {
+    input: "src/main.tsx",
+    output: {
+      file: "dist/bundle.js",
+      format: "cjs",
+      sourcemap: true,
+    },
+    plugins: [
+      replace({
+        "process.env.NODE_ENV": JSON.stringify("production"),
+        preventAssignment: true,
+      }),
+      external(),
+      resolve(),
+      commonjs(),
+      postcss({
+        inject: false, // don't inject JavaScript
+        extract: true, // write a CSS file
+        plugins: [postcssNormalize(), postcssImport(), autoprefixer()],
+        writeDefinitions: false, // true -> generate .css.d.ts files
+      }),
+      typescript({ tsconfig: "./tsconfig.json" }),
+      serve("dist"),
+      sourcemaps(),
+      buildHTML(),
+    ].concat(envPlugins),
   },
-  plugins: [
-    replace({
-      "process.env.NODE_ENV": JSON.stringify("production"),
-      preventAssignment: true,
-    }),
-    external(),
-    resolve(),
-    commonjs(),
-    postcss({
-      inject: false, // don't inject JavaScript
-      extract: true, // write a CSS file
-      plugins: [postcssNormalize(), postcssImport(), autoprefixer()],
-      // minimize: true,
-      writeDefinitions: false, // true -> generate .css.d.ts files
-    }),
-    typescript({ tsconfig: "./tsconfig.json" }),
-    serve("dist"),
-    sourcemaps(),
-    livereload(),
-  ],
-};
+];
